19ab05b4677f6d64e88bbf3ee90109afa558b9bb
Nebeolisa Nnamdi <nebnnamdi@gmail.com>
2022-08-24 08:16:32 -0700

    isupper

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def uppercase(str):
-    for i in str:
-        if ord(i) >= ord('a') and ord(i) <= ord('z'):
-            i = chr(ord(i) - 32)
-    print("{:s}".format(i), end="")
-    print()
+    for c in range(len(str)):
+        n = ord(str[c])
+        if (n >= 97 and n <= 122):
+            n -= 32
+        print("{}".format(chr(n)), end="")
+        print()
