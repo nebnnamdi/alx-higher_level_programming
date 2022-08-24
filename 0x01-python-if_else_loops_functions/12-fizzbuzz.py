b4d8e795407d090552dab8ef9872808a289a78c3
Nebeolisa Nnamdi <nebnnamdi@gmail.com>
2022-08-24 09:00:52 -0700

    fizzbuzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -8,4 +8,4 @@ def fizzbuzz():
         elif (n % 5 == 0 and n % 3 != 0):
             print("Buzz", end=" ")
         else:
-             print("{:d}".format(n), end=" ")
+            print("{:d}".format(n), end=" ")
