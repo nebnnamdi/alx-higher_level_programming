beac584c095b692aa33c1760f2102c0e7d3051b8
Nebeolisa Nnamdi <nebnnamdi@gmail.com>
2022-08-25 08:48:08 -0700

    Infinite addition

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,17 +1,10 @@
 #!/usr/bin/python3
-def add_arg(argv):
-    n = len(argv) - 1
-    if n == 0:
-        print("{:d}".format(n))
-        return
-    else:
-        i = 1
-        add = 0
-        while i <= n
-            add += int(argv[i])
-            i += 1
-        print("{:d}".format(add))
-
 if __name__ == "__main__":
     import sys
-    add_arg(sys.argv)
+    res = 0
+    for str in range(len(sys.argv)):
+        if (str == 0):
+            continue
+        else:
+            res += int(sys.argv[str])
+    print("{}".format(res))
