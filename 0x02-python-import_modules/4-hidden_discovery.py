ba9a7f81f8df4cea19406778bc3ded9ba4b72c3b
Nebeolisa Nnamdi <nebnnamdi@gmail.com>
2022-08-25 08:55:46 -0700

    who are you

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import hidden_4
+
+def discovery():
+    name = dir(hidden_4)
+    for i in name:
+        if i[:2] != '__':
+            print("{:s}".format(i))
+
+if __name__ == "__main__":
+    discovery()
