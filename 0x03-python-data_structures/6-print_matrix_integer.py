19fed34ee984164074d5ec5cec64965ca1390fb9
Nebeolisa Nnamdi <nebnnamdi@gmail.com>
2022-08-27 14:37:07 -0700

    6 & 7

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for x in matrix:
+        a = ""
+        for y in x:
+            print("{:a}{:d}".format(a,b), end="")
+            a = " "
+        print()
