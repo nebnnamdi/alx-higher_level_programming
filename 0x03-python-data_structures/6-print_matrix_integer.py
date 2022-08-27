c114993af4d487a22bac4233870f8a2f09023163
Nebeolisa Nnamdi <nebnnamdi@gmail.com>
2022-08-27 14:40:34 -0700

    6 correction

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for x in matrix:
-        a = ""
-        for y in x:
-            print("{:a}{:d}".format(a,b), end="")
-            a = " "
-        print()
+        print(' '.join('{:d}'.format(j)for y in x))
