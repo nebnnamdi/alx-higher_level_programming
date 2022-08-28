a8f6523d5d5c14185fc1118df97c8079c0e1f7d3
Nebeolisa Nnamdi <nebnnamdi@gmail.com>
2022-08-28 15:30:10 -0700

    only by 2

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if len(my_list) == 0:
-        return ("None")
-    x = my_list[0]
+    new_list = []
     for i in my_list:
-        if i > x:
-            x = i
-        return (x)
+        if i % 2 == 0:
+            new_list.append(True)
+        else:new_list.append(False)
+    return new_list
