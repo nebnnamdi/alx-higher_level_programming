00afbb7bb9cb99820865539a133f32f579b74327
Nebeolisa Nnamdi <nebnnamdi@gmail.com>
2022-08-31 07:13:19 -0700

    biggest score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,5 +1,8 @@
 #!/usr/bin/pyton3
 def best_score(a_dictionary):
-    if not a_dictionary:
+    if a_dictionary == {} or is None:
         return None
-    return max(a_dictionary, keys=a_dictionary.get)
+    biggest = max(a_dictionary.values))
+    for keys, values in a_dictionary:
+        if values = biggest:
+            return keys
