c4f7bbdb6db185fdb7a172925393a3dbce7ed0ae
Nebeolisa Nnamdi <nebnnamdi@gmail.com>
2022-09-12 08:03:50 -0700

    safe print division

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,11 @@
+!#/bin/usr/python3
+def safe_print_division(a, b):
+    result = 0
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        result = None
+    finally:
+        print("Inside result: ", end="")
+    print("{}".format(result))
+    return result
