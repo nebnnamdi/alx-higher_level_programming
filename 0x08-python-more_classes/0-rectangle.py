18ced0e3b9cfee4ebf0935fe2e5aec9130b4f53e
Nebeolisa Nnamdi <nebnnamdi@gmail.com>
2022-09-19 05:25:26 -0700

    rectangle

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """
-A rectangle class
+Class Rectangle: Defines a Rectangle
 """
 
+
 class Rectangle:
-    """Empty Rectangle Class"""
+    """ Empty class of Rectangle """
     pass
--- 0x08-python-more_classes/0-square.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""
-Class Square: Defines a square
-"""
-
-
-class Square:
-    """ Makes an empty Square Class """
-    pass
