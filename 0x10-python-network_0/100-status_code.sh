c6626c53e4c6477c67622a3ca4be439a354ba742
Nebeolisa Nnamdi <nebnnamdi@gmail.com>
2022-12-15 14:05:28 -0800

    adv

--- 0x10-python-network_0/100-status_code.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# Script that sends a request and displays only the status code of the response.
-curl -s "$1" -o /dev/null -w "%{http_code}"
--- 0x10-python-network_0/101-post_json.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-#  Script that sends a JSON POST request and displays the body of the response.
-curl -s "$1" -X POST -H "Content-Type: application/json" -d @"$2"
