9337f16e897dd28462a6dfc71a84e12a03921741
Nebeolisa Nnamdi <nebnnamdi@gmail.com>
2022-12-15 14:13:51 -0800

    adv

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Script that causes the server to respond with a message containing You got me!, in the body of the response
-curl -sL 0.0.0.0:5000/catch_me -X PUT -H "Origin:HolbertonSchool" -d "user_id=98"
+# Script that makes a request to 0.0.0.0:5000/catch_me and get the response
+curl -sX PUT -L -d "user_id=98" -H "Origin:HolbertonSchool" "0.0.0.0:5000/catch_me"
